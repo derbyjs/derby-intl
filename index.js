6f3da7d403e92374ea2bd457b944515f926dd142
rachael <rkstedman@users.noreply.github.com>
2015-01-09 11:12:01 -0800

    stubbed out functions

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = function(app) {
+  app.proto.formatDate = formatDate;
+  app.proto.formatTime = formatTime;
+  app.proto.formatRelative = formatRelative;
+  app.proto.formatNumber = formatNumber;
+  app.proto.formatMessage = formatMessage;
+}
+
+function formatDate(date, locale, options) {
+
+};
+
+function formatTime(date, locale, options) {
+
+};
+
+function formatRelative(date, locale, options) {
+
+};
+
+function formatNumber(date, locale, options) {
+
+};
+
+function formatMessage(date, locale, options) {
+
+};
